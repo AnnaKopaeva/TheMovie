c5c2f9349fec6e7bdfbcc3deebccf159d2680d27
Anna Kopaieva <ann.kopaieva@gmail.com>
2019-04-19 10:21:03 +0300

    Updates

---
+++ precache-manifest.3145a73c2f6f1da9f37975b9aecd6bbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53f9236bfdf36196423f29e78be417b0",
+    "url": "/theMovie/static/media/linkedin.53f9236b.png"
+  },
+  {
+    "revision": "faf68a37ecd116e8f7ab",
+    "url": "/theMovie/static/js/runtime~main.2f89188a.js"
+  },
+  {
+    "revision": "cd2d1c09222ffb3e528e",
+    "url": "/theMovie/static/js/main.90d04a9e.chunk.js"
+  },
+  {
+    "revision": "a357b2ee232d97a7554b",
+    "url": "/theMovie/static/js/2.2fd9dd05.chunk.js"
+  },
+  {
+    "revision": "cd2d1c09222ffb3e528e",
+    "url": "/theMovie/static/css/main.273b8b01.chunk.css"
+  },
+  {
+    "revision": "9929191b9284e3a6e9304e8390883a5c",
+    "url": "/theMovie/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/theMovie/precache-manifest.f55b4f4a97d80646588af828ae30b522.js"
+  "/theMovie/precache-manifest.3145a73c2f6f1da9f37975b9aecd6bbe.js"
 );
 
 workbox.clientsClaim();
